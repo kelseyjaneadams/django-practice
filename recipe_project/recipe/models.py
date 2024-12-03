c46678375dd3408b0ba8d273faf56ffd27ede2b6
Kelsey Jane Adams <kelseyjaneadams3@gmail.com>
2024-12-03 15:15:27 +0000

    Create the Django application models

---
+++ recipe_project/recipe/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+# name: A CharField for the recipe's name (max 200 characters).
+# ingredients: A TextField for listing the ingredients.
+# instructions: A TextField for the preparation instructions.
+# __str__(self): A method that returns the recipe's name when the object is printed or displayed.
+# The CharField type is used for shorter text fields with a maximum length, while TextField is suitable for longer text content without predefined limits.
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+
+    def __str__(self):
+        return self.name
